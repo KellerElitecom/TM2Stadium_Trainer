fd0eecff5d3e2f74cd556f814f37f0c29b108403
KellerElitecom <admin@keller-elite.com>
2014-05-08 20:10:07 +0200

    Fixing Source (all)

---
+++ TM2Stadium_Trainer/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    Private Sub time_check_Tick(sender As Object, e As EventArgs) Handles time_check.Tick
+        If UpdateProcessHandle() Then
+            lbl_status.Text = "TM2 found!"
+            lbl_status.ForeColor = Color.LimeGreen
+        Else
+            lbl_status.Text = "Waiting for TM2..."
+            lbl_status.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        RemoveProtection(&H400310, 100)
+
+    End Sub
+End Class
